9fc8bd88eb0be0f5a597e4c6a60d6ba8353ff6a3
Abhinand <wabhinandj@gmail.com>
2025-12-28 23:55:10 -0800

    coded service-worker as per comments

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,10 @@
 import blockList from "./block-list.js";
 
+// global state. determines whether to block or not.
 let state = false;
 
-// dict to store blocked tabids & urls
+// map to store blocked tabids & urls
+const map = Map();
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.type == "GET_STATE") {
@@ -13,22 +15,49 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         // this is where i block/unblock existing tabs
         // if block: for each tab: if part of blacklist: add tabid, url to dict; redirect tab to block page
         // if unblock: for each tabid, url in dict: redirect tab to url
+        if (state) {
+            chrome.tabs.query({}, (tabs) => {
+                for (const tab of tabs) {
+                    if (!tab.id) continue;
+                    for (const elem of blockList) {
+                        if (tab.url.includes(elem)) {
+                            map.set(tab.id, tab.url);
+                            chrome.tabs.update(tab.id, {url: chrome.runtime.getURL('blocked.html')});
+                            break;
+                        }
+                    }
+                }
+            });
+        } else {
+            for (const [key, val] of map) {
+                chrome.tabs.update(key, {url: val});
+            }
+            map.clear();
+        }
     }
     // popup will send updated list here, which replaces old. currently not implemented by popup so will never be called.
+    // this is not preserved between browser loads. only the initial list hardcoded in block-list.js is.
     if (msg.type == "UPDATE_BLOCKLIST" && msg.value) {
         console.log("here is where i would update blocklist");
-        blockList = msg.value;  // this is not preserved between browser loads. only the initial list hardcoded in block-list.js is. 
+        blockList = msg.value;
     }
 });
 
-// this will have to change. currently its onUpdated delete (for new tabs)
-// i would have it be onCreated -> onUpdated delete
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (state && changeInfo.status === "complete") {
-        for (const elem of blockList) {
-            if (tab.url.includes(elem)) {
-                chrome.tabs.remove(tab.id);
-            }S
+// handles new tab creation. if tab url contains blacklisted strings, removes tab.
+chrome.tabs.onCreated.addListener((tab) => {
+    if (state) {
+        const tabId = tab.id;
+
+        function handleUpdate(updatedTabId, changeInfo, updatedTab) {
+            if (updatedTabId !== tabId) return;
+            if (changeInfo.url) {
+                // delete
+                for (const elem of blockList) {
+                    if (changeInfo.url.includes(elem)) {
+                        chrome.tabs.remove(tabId);
+                    }
+                }
+            }
         }
     }
 });
