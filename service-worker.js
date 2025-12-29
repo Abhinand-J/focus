a12bd2486ff91de3c6c33e671ca5766f62260b81
Abhinand <wabhinandj@gmail.com>
2025-12-28 23:28:04 -0800

    fixed logic

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ import blockList from "./block-list.js";
 
 let state = false;
 
-// function to block existing tabs.
+// dict to store blocked tabids & urls
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.type == "GET_STATE") {
@@ -10,7 +10,9 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     }
     if (msg.type == "SET_STATE") {
         state = msg.value;
-        // this is where i block existing tabs
+        // this is where i block/unblock existing tabs
+        // if block: for each tab: if part of blacklist: add tabid, url to dict; redirect tab to block page
+        // if unblock: for each tabid, url in dict: redirect tab to url
     }
     // popup will send updated list here, which replaces old. currently not implemented by popup so will never be called.
     if (msg.type == "UPDATE_BLOCKLIST" && msg.value) {
@@ -21,7 +23,6 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 
 // this will have to change. currently its onUpdated delete (for new tabs)
 // i would have it be onCreated -> onUpdated delete
-// another listener would be onUpdated (or something to do with background tabs) redirect to block page
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (state && changeInfo.status === "complete") {
         for (const elem of blockList) {
